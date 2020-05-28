f208629b1b563c82f70e2c438a611429fbcd3ef9
Petter Lopez <petterlopez003@gmail.com>
2020-05-28 00:22:17 -0400

    improvements to data structure

--- e-app/src/App.js
+++ e-app/src/App.js
@@ -1,25 +1,63 @@
+// <!-- Time: 01:06:00 from 5:06:24 -->
 import React from 'react';
-import logo from './logo.svg';
+import data from './data';
 import './App.css';
 
 function App() {
+  const openMenu = ()=>{
+    document.querySelector(".sidebar").classList.add("open");
+}
+  const closeMenu = ()=>{
+    document.querySelector(".sidebar").classList.remove("open");
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="grid-container">
+            <header className="header">
+                <div className="brand">
+                    <button onClick={openMenu}>&#9776;</button>
+                    <a href="index.html">amazonia</a>
+                </div>
+                <div className="header-links">
+                    <a href="cart.html">Cart</a>
+                    <a href="signin.html">Sign In</a>
+                </div>
+            </header>
+            <aside className="sidebar">
+                <h3>Shopping Category</h3>
+                <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+                <ul>
+                    <li>
+                        <a href="">Shirts </a>
+                    </li>
+                    <li>
+                        <a href="">Pants</a>
+                    </li>
+                </ul>
+            </aside>
+            <main className="main"> 
+                <div className="content">
+                    <ul className="products">
+                      {
+                        data.products.map( product => 
+                          <li>
+                          <div className="product">
+                              <img className="product-img" src={product.images} alt="product"/>
+                              <div className="product-name"><a href="product.html">{product.name}</a></div>
+                              <div className="product-brand">{product.brand}</div>
+                        <div className="product-price">${product.price}</div>
+                        <div className="product-rating">{product.rating} Stars ({product.numReviews})</div>
+
+                          </div>
+                      </li>
+                        )
+                      }
+                    </ul>
+                </div>
+            </main>
+            <footer className="footer">
+                All rights reserved
+            </footer>
+        </div>
   );
 }
 
---
+++ e-app/src/data.js
@@ -0,0 +1,42 @@
+export default {
+    products:
+        [
+            {
+                name: 'Slim Shirt',
+                category: 'Shirts',
+                images: 'https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+                price: 60,
+                brand: 'Nike',
+                rating: 4.5,
+                numReviews: 10
+            },
+            {
+                name: 'Slim Shirt',
+                category: 'Shirts',
+                images: 'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?cs=srgb&dl=man-in-white-v-neck-t-shirt-and-black-pants-775358.jpg&fm=jpg',
+                price: 40,
+                brand: 'Nike',
+                rating: 4,
+                numReviews: 101
+            },
+            {
+                name: 'Slim Pants',
+                category: 'Pants',
+                images: 'https://images.pexels.com/photos/1436289/pexels-photo-1436289.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+                price: 80,
+                brand: 'Nike',
+                rating: 5,
+                numReviews: 200
+            },
+            {
+                name: 'Jacket',
+                category: 'Shirts',
+                images: 'https://images.pexels.com/photos/1124468/pexels-photo-1124468.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+                price: 160,
+                brand: 'American Eagle',
+                rating: 4.5,
+                numReviews: 50
+            }
+
+        ]
+}
