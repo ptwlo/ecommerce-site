df18e8ab4b6d20f7fd321a01ad19e0f8ea1d10e0
Petter Lopez <petterlopez003@gmail.com>
2020-05-29 20:23:17 -0400

    added dependencies: express, babel and nodemon

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+ const express = require('express');
+ import data from '../e-app/src/data';
+ const app = express();
+
+ app.get('/api/products', (req, res)=>{
+    res.send(data.products);
+ });
+
+ app.listen(5000, ()=> console.log("Server started at port 5000"));
